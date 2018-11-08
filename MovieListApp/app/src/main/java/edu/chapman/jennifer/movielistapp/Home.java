66714481f1323aeaaf939f2e02cad8d2c7058125
JenniferMoy <jennifermoy.394@gmail.com>
2018-11-07 18:20:36 -0800

    Add MovieListApp

---
+++ MovieListApp/app/src/main/java/edu/chapman/jennifer/movielistapp/Home.java
@@ -0,0 +1,15 @@
+package edu.chapman.jennifer.movielistapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homescreen);
+
+
+    }
+}
